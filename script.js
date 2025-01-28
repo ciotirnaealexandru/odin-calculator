2e86ebf83f7522dbff504e4ba27c005df54d6bbc
Alexandru-Cosmin Ciotirnae <ciotirnae.alexandru005@gmail.com>
2025-01-28 12:24:23 +0200

    Switch to string numbers but with bugs
    Signed-off-by: Alexandru-Cosmin Ciotirnae <ciotirnae.alexandru005@gmail.com>

--- script.js
+++ script.js
@@ -21,189 +21,217 @@ let sign = document.querySelector(".sign");
 let clear = document.querySelector(".AC");
 let equals = document.querySelector(".equals");
 let question = document.querySelector(".question");
+let comma = document.querySelector(".comma");
 
-let firstNumber = 0, operation, operationState = false, secondNumber = null, errorFlag = false;
+let firstNumber = "0", secondNumber = "", secondNumberState = false;
+let operation, operationState = false;
+let errorState = false;
+let firstCommaState = false, secondCommaState = false;
 
 function clearEcuation () {
-    firstNumber = 0;
-    secondNumber = null;
+    firstNumber = "0";
+    secondNumber = "";
     operationState = false;
+    secondNumberState = false;
     output.textContent = "0";
 }
 
 function displayError (string) {
     clearEcuation();
-    errorFlag = true;
+    errorState = true;
     output.textContent = string;
 }
 
-function add (a, b) {
-    return a + b;
+function add (firstString, secondString) {
+    let a = parseInt(firstString);
+    let b = parseInt(secondString);
+
+    return (a + b).toString();
 }
 
-function subtract (a, b) {
-    return a - b;
+function subtract (firstString, secondString) {
+    let a = parseInt(firstString);
+    let b = parseInt(secondString);
+
+    return (a - b).toString();
 }
 
-function multiply (a, b) {
-    return a * b;
+function multiply (firstString, secondString) {
+    let a = parseInt(firstString);
+    let b = parseInt(secondString);
+
+    return (a * b).toString();
 }
 
-function divide (a, b) {
+function divide (firstString, secondString) {
+    let a = parseInt(firstString);
+    let b = parseInt(secondString);
+
     if (b == 0)
         return "Pe bune?"
-    return Math.round(a / b * 1000) / 1000;
+    else
+        return (Math.round(a / b * 1000) / 1000).toString();
 }
 
-function mod(a, b) {
+function mod(firstString, secondString) {
+    let a = parseInt(firstString);
+    let b = parseInt(secondString);
+
     if (b == 0)
         displayError("Serios?");
     else if (a < 0 || b < 0 || isNaN(a) || isNaN(b))
         displayError("Invalid input");
     else
-        return Math.round(a % b * 1000) / 1000;
+        return (Math.round(a % b * 1000) / 1000).toString();
 }
 
 function outputEcuation () {
-    let equation = firstNumber.toString();
-    if (operationState == true) {
-        equation += " " + operation + " ";
-    }
-    if (secondNumber != null) {
-        equation += secondNumber.toString();
-    }
+    let equation = firstNumber;
+
+    if (operationState == true)
+        equation += " " + operation;
+
+    if (secondNumberState == true)
+        equation += " " + secondNumber;
 
     output.textContent = equation;
 }
 
 function addDigit (digit) {
-    //firstNumber
-    if (operationState == false){
-        if(firstNumber == 0)
-            firstNumber =  digit;
-        else
-            firstNumber = firstNumber * 10 + digit;
-    }
-    //secondNumber
-    else {
-        if(secondNumber == null)
-            secondNumber =  digit;
-        else
-            secondNumber = secondNumber * 10 + digit;
-    }
+    if (errorState == false) {
+        //firstNumber
+        if (operationState == false) {
+            if(firstNumber == "0")
+                firstNumber = digit;
+            else
+                firstNumber = firstNumber + digit;
+        }
+        //secondNumber
+        else {
+            if (secondNumberState == false || secondNumber == "0") {
+                secondNumberState = true;
+                secondNumber = digit;
+            }
+            else
+                secondNumber = secondNumber + digit;
+        }
 
-    outputEcuation();
+        outputEcuation();
+    }
+}
+
+function addComma () {
+    
 }
 
 function operate () {
-    if (operationState == true) {
-        if (operation == "+") {
-            firstNumber = add(firstNumber, secondNumber);
-            secondNumber = null;
-        }
+    if (errorState == false) {
+        if (operationState == true) {
+            if (operation == "+") {
+                firstNumber = add(firstNumber, secondNumber);
+                secondNumber = "";
+            }
 
-        if (operation == "-") {
-            firstNumber = subtract(firstNumber, secondNumber);
-            secondNumber = null;
-        }
+            if (operation == "-") {
+                firstNumber = subtract(firstNumber, secondNumber);
+                secondNumber = "";
+            }
 
-        if (operation == "*") {
-            firstNumber = multiply(firstNumber, secondNumber);
-            secondNumber = null;
-        }
+            if (operation == "×") {
+                firstNumber = multiply(firstNumber, secondNumber);
+                secondNumber = "";
+            }
 
-        if (operation == "/") {
-            firstNumber = divide(firstNumber, secondNumber);
-            secondNumber = null;
-        }
+            if (operation == "÷") {
+                firstNumber = divide(firstNumber, secondNumber);
+                secondNumber = "";
+            }
 
-        if (operation == "%") {
-            firstNumber = mod(firstNumber, secondNumber);
-            secondNumber = null;
+            if (operation == "%") {
+                firstNumber = mod(firstNumber, secondNumber);
+                secondNumber = "";
+            }
         }
     }
-
-    if (errorFlag == false)
+    if (errorState == false)
         output.textContent = firstNumber;
-    else
-        errorFlag = false;
 }
 
 function changeOperation (newOperation) {
-    if (errorFlag == false) {
+    if (errorState == false) {
         if (operationState == false) {
             operationState = true;
             operation = newOperation;
         }
-        else {
-            if (secondNumber != null)
-                operate();
+        else if (secondNumberState == true) {
+            operate();
             operation = newOperation;
         }
+        else
+            operation = newOperation;
+        
         outputEcuation();
     }
 }
 
 zero.addEventListener("click", () => {
-    addDigit(0);
+    addDigit("0");
 });
 one.addEventListener("click", () => {
-    addDigit(1);
+    addDigit("1");
 });
 two.addEventListener("click", () => {
-    addDigit(2);
+    addDigit("2");
 });
 three.addEventListener("click", () => {
-    addDigit(3);
+    addDigit("3");
 });
 four.addEventListener("click", () => {
-    addDigit(4);
+    addDigit("4");
 });
 five.addEventListener("click", () => {
-    addDigit(5);
+    addDigit("5");
 });
 six.addEventListener("click", () => {
-    addDigit(6);
+    addDigit("6");
 });
 seven.addEventListener("click", () => {
-    addDigit(7);
+    addDigit("7");
 });
 eight.addEventListener("click", () => {
-    addDigit(8);
+    addDigit("8");
 });
 nine.addEventListener("click", () => {
-    addDigit(9);
+    addDigit("9");
 });
 
 addButton.addEventListener("click", () => {
     changeOperation("+");
 });
-
 subtractButton.addEventListener("click", () => {
     changeOperation("-");
 });
-
 multiplyButton.addEventListener("click", () => {
-    changeOperation("*");
+    changeOperation("×");
 });
-
 divideButton.addEventListener("click", () => {
-    changeOperation("/");
+    changeOperation("÷");
 });
-
 modButton.addEventListener("click", () => {
     changeOperation("%");
 });
 
 clear.addEventListener("click", () => {
     clearEcuation();
+    errorState = false;
 });
-
 equals.addEventListener("click", () => {
     operate();
     operationState = false;
 });
-
+comma.addEventListener("click", () => {
+    addComma();
+});
 question.addEventListener("click", () => {
     displayError("Doi scheleti se trag de...");
 });
